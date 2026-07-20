453fb349a99ff6b47fdafe3c1f856099e5f3f52f
Your Name <you@example.com>
2026-07-20 10:13:45 -0300

    Primera entrega

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Asignación de un valor
+let nombre = "luciano"; // dato string
+nombre = "matias";
+nombre = "pepito";
+
+const PI = 3.14; // dato number
+
+console.log(2 + 2);
+
+let esVerdadero = false; // boolean: true o false
+
+nombre = prompt("Ingresá tu nombre");
+let apellido = prompt("Ingresá tu apellido");
+let edad = parseInt(prompt("Ingresá tu edad"));
+
+alert("Hola " + nombre);
+
+console.log("Hola " + nombre);
+
+// Suma
+let edadProximoAnio = edad + 1;
+
+let mensaje = "Hola " + nombre + " " + apellido + ". Tenés " + edad + " años. El próximo año vas a tener " + edadProximoAnio + " años.";
+
+console.log(mensaje);
+alert(mensaje);
